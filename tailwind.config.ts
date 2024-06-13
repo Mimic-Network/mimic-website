caf2db724f3e1b05d38b82bfe88cdb81596eab13
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-06-13 22:33:32 +0100

    chore: setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,16 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-glassy":
+          "linear-gradient(90deg,rgba(255,255,255,0.1) 40%,rgba(255,255,255,0.1) 100%)",
+      },
+      colors: {
+        primary: {
+          900: "#2F58B0",
+        },
+        secondary: {
+          900: "#4A4A4A"
+        }
       },
     },
   },
