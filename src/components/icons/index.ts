ec3455d54eea818cc36a6b88c6b20f27144930d7
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-06-17 20:04:45 +0100

    feat: exported icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as AbstractLeft } from "./AbstractLeft.icon";
+export { default as AbstractRight } from "./AbstractRight.icon";
+export { default as AbstractMiddle } from "./AbstractMiddle.icon";
