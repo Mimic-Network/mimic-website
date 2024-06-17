6e1e57764cb0c6ace485873665bbb5d07a17bf46
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-06-17 20:06:57 +0100

    feat: created button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
 import { twMerge } from "tailwind-merge";
 
-export interface IButtonProps {
+export interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   className?: string;
 }
 
-const Button: React.FC<IButtonProps> = props => {
-  return <section className={twMerge("px-8 py-3 bg-black text-white rounded-full", props.className)}>{props.children}</section>;
+const Button: React.FC<IButtonProps> = ({ children, className, ...props }) => {
+  return (
+    <section
+      className={twMerge(
+        "px-8 py-3 bg-black cursor-pointer text-white rounded-full",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </section>
+  );
 };
 export default Button;
