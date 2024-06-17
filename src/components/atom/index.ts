eec32cfc9ce1d75737c1f3a85865b472b2079bd6
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-06-17 19:18:40 +0100

    improvements: created folder export for components

---
+++ src/components/atom/index.ts
@@ -0,0 +1,12 @@
+export { default as CTA } from './CTA';
+export { default as FAQ } from './FAQ';
+export { default as Features } from './Features';
+export { default as Footer } from './Footer';
+export { default as Hero } from './Hero';
+export { default as About } from './About';
+export { default as Navbar } from './Navbar';
+export { default as Layout } from './Layout';
+export { default as Contact } from './Contact';
+export { default as PrivacyPolicy } from './PrivacyPolicy';
+export { default as TermsAndConditions } from './TermsAndConditions';
+export { default as WaitlistModal } from './WaitlistModal';
